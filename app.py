190325586545fb45526bf3444efda144d34d2c7c
RandallWyatt <randallw16@fullsail.edu>
2016-12-05 19:31:00 -0500

    removal of broken code

--- app.py
+++ app.py
@@ -11,9 +11,3 @@ if __name__ == '__main__':
     app.run()
 
 
-resp = request.get('index.html')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-for todo_item in resp.json():
-    print('{} {}'.format(todo_item['id'], todo_item['summary']))
