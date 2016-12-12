987d4d2296a962a6704e1f1aa88f6fe581640093
RandallWyatt <randallw16@fullsail.edu>
2016-12-11 19:43:28 -0500

    Added color choices

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    return render_template('index.html')
+@app.route('/', defaults={'color': 'default'})
+@app.route('/<color>', endpoint='color')
+def main(color):
+    style_link = '/static/' + color + '.css'
+    return render_template('index.html', styles=style_link)
 
 if __name__ == '__main__':
     app.run()
